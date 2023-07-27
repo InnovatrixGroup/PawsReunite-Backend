b4a08157cbf6403d25b5d5e7f3eed2e13f150259
Jiezhoue <13809@coderacademy.edu.au>
2023-07-27 16:32:13 +1000

    change title length limitation to 15

--- src/middleware/PostMiddleware.js
+++ src/middleware/PostMiddleware.js
@@ -2,7 +2,7 @@ const { body, validationResult } = require("express-validator");
 
 // using [] to group multiple middleware into one middleware
 const validateTitleLength = [
-  body("title").isLength({ max: 10 }).withMessage("Title must be less than 10 characters long"),
+  body("title").isLength({ max: 15 }).withMessage("Title must be less than 15 characters long"),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
