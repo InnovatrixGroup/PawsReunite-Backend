60636f3005f54bd5f540d9bfacf6c27152b45fcd
Jiezhoue <13809@coderacademy.edu.au>
2023-08-01 14:11:01 +1000

    update some comments

--- src/middleware/ErrorMiddleware.js
+++ src/middleware/ErrorMiddleware.js
@@ -1,3 +1,4 @@
+// This middleware checks if there are any errors in the request object.
 const errorCheck = (req, res, next) => {
   if (req.errors.length > 0) {
     return res.status(400).json({ errors: req.errors });
